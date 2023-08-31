4a115256a2872f0710e59f8b4ad525108d25ca96
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-31 22:23:57 +0300

    Feat(JS, CSS, HTML) Added scroll to top button

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import './scripts/mobile-menu';
 import './scripts/mobile-icon-open';
 import './scripts/modal-team';
 import './scripts/smooth-scroll';
+import './scripts/scroll-top';
---
+++ src/scripts/scroll-top.js
@@ -0,0 +1,27 @@
+document.addEventListener('scroll', handleScroll);
+// get a reference to our predefined button
+let scrollToTopBtn = document.querySelector('.scroll__top');
+
+function handleScroll() {
+  let scrollableHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let GOLDEN_RATIO = 0.1;
+
+  if (document.documentElement.scrollTop / scrollableHeight > GOLDEN_RATIO) {
+    //show button
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    //hide button
+    scrollToTopBtn.style.display = 'none';
+  }
+}
+
+scrollToTopBtn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
